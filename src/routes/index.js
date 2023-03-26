8fdebc615b89007b6aaed9add39f12d7aa15be19
victor <josevictornascimento2016@gmail.com>
2023-03-26 15:37:00 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const usersRoutes = require("./users.routes");
+const movieNotesRoutes = require("./movie_notes.routes");
+const movieTagsRoutes = require("./movie_tags.routes");
+const sessionsRoutes = require("./sessions.routes");
+
+const routes = Router();
+
+routes.use("/users", usersRoutes);
+routes.use("/movie_notes", movieNotesRoutes);
+routes.use("/movie_tags", movieTagsRoutes);
+routes.use("/sessions", sessionsRoutes);
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,37 @@
+require("express-async-errors");
+
+const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
+const sqliteConnection = require("./database/sqliteConnection");
+const AppError = require("./utils/AppError");
+
+sqliteConnection();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal server error"
+  })
+})
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`));
